9808892214198fbf1858f555ad6edcf497020054
offMomySon <ohji100629@naver.com>
2021-01-23 20:49:50 +0900

    141. Linked List Cycle
    https://leetcode.com/problems/linked-list-cycle/

---
+++ 141. Linked List Cycle/소스.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+class Solution {
+public:
+	bool hasCycle(ListNode* head) {
+		if (head == NULL) return false;
+
+		ListNode* slow = head;
+		ListNode* fast = head;
+
+		while (fast->next && fast->next->next) {
+			fast = fast->next->next;
+			slow = slow->next;
+
+			if (slow == fast)
+				return true;
+		}
+
+		return false;
+	}
+};
