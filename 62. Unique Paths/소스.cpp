d1f339945d57bf5b27d6d7fd9a2b8dcb39642af4
offMomySon <ohji100629@naver.com>
2021-02-19 21:33:15 +0900

    62. Unique Paths
    https://leetcode.com/problems/unique-paths/

---
+++ 62. Unique Paths/소스.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+
+	vector<vector<int>> dp;
+
+public:
+	int uniquePaths(int m, int n) {
+		dp = vector<vector<int>>(m, vector<int>(n, 0));
+		for (int i = 0; i < n; i++) {
+			dp[0][i] = 1;
+		}
+		for (int i = 0; i < m; i++) {
+			dp[i][0] = 1;
+		}
+
+
+
+
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+			}
+		}
+		
+		return dp[m - 1][n - 1];
+	}
+};
+
+int main() {
+
+	Solution *s = new Solution();
+	cout << s->uniquePaths(3, 2);
+}
