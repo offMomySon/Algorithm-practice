4644e0b55c507b9eaf054d43a47f47d01af36c66
offMomySon <ohji100629@naver.com>
2021-03-01 18:24:42 +0900

    1217. Minimum Cost to Move Chips to The Same Position
    https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/

---
+++ 1217. Minimum Cost to Move Chips to The Same Position/소스.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int even = 0, odd = 0;
+
+        for (int i = 0; i < position.size(); i++) {
+            if (position[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        return min(even, odd);
+    }
+};
+
+int main() {
+    Solution* s = new Solution();
+    vector<int> v({ 2,2,2,3,3 });
+    cout << s->minCostToMoveChips(v);
+
+}
