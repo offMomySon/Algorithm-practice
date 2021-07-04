df88f91ccfe9f7748ed5f8130db1f9b159256270
offMomySon <ohji100629@naver.com>
2021-07-05 08:07:05 +0900

    back_11764_refac

--- algorithm-369/back_1764/src/main/java/Main.java
+++ algorithm-369/back_1764/src/main/java/Main.java
@@ -13,22 +13,45 @@ public class Main {
         M = fs.nextInt();
 
         result_name = new ArrayList<>();
-        N_group = new String[N];
-        M_group = new String[M];
+        N_group = new String[N+1];
+        M_group = new String[M+1];
 
-        for (int i = 0; i <= N-1; i++) N_group[i] = fs.readLine();
-        for (int i = 0; i <= M-1; i++) M_group[i] = fs.readLine();
+        for (int i = 1; i <= N; i++) N_group[i] = fs.readLine();
+        for (int i = 1; i <= M; i++) M_group[i] = fs.readLine();
+    }
+
+    public static int binarySearch(String arr[], int left, int right, String target){
+        int res = left;
+
+        while(left<=right){
+            int mid = (left+right)/2;
+
+            int compareResult = arr[mid].compareTo(target);
+            if(compareResult == 0){
+                return mid;
+            }
+
+            if(compareResult < 0 ){
+                left = mid+1;
+            }
+            else if (compareResult > 0){
+                right = mid-1;
+            }
+        }
+
+        return res-1;
     }
 
     public static void main(String[] args) {
         input();
 
-        Arrays.sort(M_group);
+        Arrays.sort(M_group,1, M+1);
 
-        for (int i = 0; i < N_group.length; i++) {
-            int resultIndex = Arrays.binarySearch(M_group,N_group[i]);
+        for (int i = 1; i < N_group.length; i++) {
+//            int resultIndex = Arrays.binarySearch(M_group,1, M_group.length,N_group[i]);
+            int resultIndex = binarySearch(M_group,1, M_group.length-1,N_group[i]);
 
-            if( resultIndex >=0){
+            if( resultIndex >=1){
                 result_name.add(M_group[resultIndex]);
             }
         }
