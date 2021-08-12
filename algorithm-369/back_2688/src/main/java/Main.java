6c850fbf5a1914f28952df87556df8c1914331af
offMomySon <ohji100629@naver.com>
2021-08-12 19:19:32 +0900

    back_2688

---
+++ algorithm-369/back_2688/src/main/java/Main.java
@@ -0,0 +1,103 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+//날짜 - 2021/08/11
+//시작 - 20:22
+//종료 - 02:30
+//실수 -
+//참조 -
+
+public class Main {
+    private static FastReader fr = new FastReader();
+    private static StringBuilder sb = new StringBuilder();
+    private static long[][] dp;
+    private static int N;
+
+    public static void input(){
+        N = fr.nextInt();
+
+        dp = new long[N+1][1001];
+
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        dp[1][3] = 1;
+        dp[1][4] = 1;
+        dp[1][5] = 1;
+        dp[1][6] = 1;
+        dp[1][7] = 1;
+        dp[1][8] = 1;
+        dp[1][9] = 1;
+    }
+
+    public static void pro(){
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j <=9; j++) {
+                for (int inner = 0; inner <= j; inner++) {
+                    dp[i][j] += dp[i-1][inner];
+                }
+            }
+        }
+
+        long sum = 0;
+        for (int i = 0; i <= 9; i++) {
+            sum += dp[N][i];
+        }
+        sb.append(sum).append("\n");
+    }
+
+    public static void main(String[] args) {
+        int loop = fr.nextInt();
+
+        for (int i = 0; i < loop; i++) {
+            input();
+            pro();
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public static class FastReader{
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        public FastReader(String file) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(file)));
+        }
+
+        private String next(){
+            while( st == null || !st.hasMoreElements()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt(){
+            return Integer.parseInt(next());
+        }
+        public long nextLong(){
+            return Long.parseLong(next());
+        }
+        public double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        public String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
+
